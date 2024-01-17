cb5fb4dcc977dffb8700d1f836c808050d658d4d
Pedro <pdihax@gmail.com>
2024-01-17 11:08:22 -0300

    Add Integration Testing

--- vcsl_api/main.py
+++ vcsl_api/main.py
@@ -5,31 +5,6 @@ from di_init import init_di
 from routers.rout_health import HealthCheckRouter
 from routers.rout_bitarray import BitArrayRouter
 
-from persistance.datastore_postgres import PostgresDataStore
-
-
-from services.serv_bitarray import BitArrayService
-# import asyncio
-
-
-async def test():
-    bit_array_service = di[BitArrayService]
-    uuid = await bit_array_service.create_bit_array()
-    # await bit_array_service.flip_bit(uuid, 10)
-    index = await bit_array_service.acquire_bit_array_index(uuid)
-    if index == -1:
-        print("No free bits")
-        return
-
-    await bit_array_service.flip_bit(uuid, index)
-    bit_array = await bit_array_service.get_bit_array(uuid)
-    bits = []
-    for i in range(index - 5, index + 5):
-        bits.append((i, bit_array[i]))
-
-# asyncio.run(test())
-
-
 app = FastAPI()
 
 
--- vcsl_api/test_all.py
+++ vcsl_api/test_all.py
@@ -1,6 +1,18 @@
-def test_health():
-    assert 1 == 1
+# Integration testing for the entire project
+# Needs a Postgres database running on localhost:5432
+# Needs a Redis server running on localhost:6379
+import requests
+import pytest
 
 
-def test_create_bitarray():
-    assert 1 == 1
+def test_create_bit_array():
+    path = '/bit-array'
+    # Create a bit array
+    response = requests.put(pytest.api_url + path)
+    assert response.status_code == 200
+    assert 'id' in response.json()
+    print(response.json())
+
+
+def __test_flip_bit():
+    pass
