fc9db59e4526f09ab4709c653c9a8a73f1e1fe69
Pedro <pdihax@gmail.com>
2024-01-17 16:56:38 -0300

    Add Web3 smart contract and endpoints

--- vcsl_api/di_init.py
+++ vcsl_api/di_init.py
@@ -3,6 +3,7 @@ from os import environ
 from persistance.datastore_postgres import PostgresDataStore
 from services.abstractClasses.serv_cache_i import ICacheService
 from services.abstractClasses.serv_lock_i import ILockService
+from services.serv_web3 import Web3Service
 from services.serv_lock import LockService
 from services.serv_cache import CacheService
 from redis import Redis
@@ -22,6 +23,15 @@ def init_di() -> None:
     connected = di[PostgresDataStore].init_connections()
     if not connected:
         raise Exception("Unable to connect to postgres")
+
+    di['web3_url'] = environ.get('WEB3_URL', 'http://localhost:7545')
+    di['web3_wallet_priv_key'] = environ.get('WEB3_PRIVATE_KEY', '0x78f20d29b9ff7226d40fd408d6a991aac2af4364b5e1a31b5a96035dcf68f44b')  # This is a development only key
+    di['web3_contract_addr'] = environ.get('WEB3_CONTRACT_ADDR', '0x8ceBE8333A85Faed0e55EB2c07607A491cdA66f9')
+    di['web3_abi_path'] = environ.get('WEB3_ABI_PATH', './resources/smart_contracts/vcsl_abi.json')
+    with open(di['web3_abi_path'], 'r') as abi_definition:
+        di['web3_abi'] = abi_definition.read()
+
+    di[Web3Service] = Web3Service(url=di['web3_url'], wallet_priv_key=di['web3_wallet_priv_key'], contract_addr=di['web3_contract_addr'], abi=di['web3_abi'])
 
     di[ICacheService] = CacheService()
     di[ILockService] = LockService()
--- vcsl_api/main.py
+++ vcsl_api/main.py
@@ -4,6 +4,7 @@ from kink import di
 from di_init import init_di
 from routers.rout_health import HealthCheckRouter
 from routers.rout_bitarray import BitArrayRouter
+from routers.rout_web3 import Web3Router
 
 app = FastAPI()
 
@@ -13,5 +14,7 @@ def startup_event():
     init_di()
     health_router = di[HealthCheckRouter]
     bit_array_router = di[BitArrayRouter]
+    web3_router = di[Web3Router]
     app.include_router(health_router.router)
     app.include_router(bit_array_router.router)
+    app.include_router(web3_router.router)
